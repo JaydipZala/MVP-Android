c2df76b94efea8446d8870c7d14af89233809d33
Jaydip Zala <jaydip.z@latitudetechnolabs.com>
2016-11-10 18:10:38 +0530

    Initial commit

---
+++ app/src/main/java/com/mvpexample/app/login/LoginInteractorImpl.java
@@ -0,0 +1,31 @@
+package com.mvpexample.app.login;
+
+import android.os.Handler;
+import android.text.TextUtils;
+
+public class LoginInteractorImpl implements LoginInteractor {
+
+    @Override
+    public void login(final String username, final String password, final OnLoginFinishedListener listener) {
+        // Mock login. I'm creating a handler to delay the answer a couple of seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                boolean error = false;
+                if (TextUtils.isEmpty(username)) {
+                    listener.onUsernameError();
+                    error = true;
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    listener.onPasswordError();
+                    error = true;
+                    return;
+                }
+                if (!error) {
+                    listener.onSuccess();
+                }
+            }
+        }, 2000);
+    }
+}
